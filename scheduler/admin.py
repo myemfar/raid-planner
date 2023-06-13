181cdc9f384ecc29c8c7047537567fc77e850577
Bryan Fischer <myemfar@gmail.com>
2023-06-13 11:45:46 -0600

    added character basics

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from scheduler.models import WowClass, WowRole, Character, WowSecondaryRole
 
-# Register your models here.
+
+@admin.register(WowClass)
+class WowClassAdmin(admin.ModelAdmin):
+    list_display = (
+        "character_class",
+    )
+
+@admin.register(WowRole)
+class WowRoleAdmin(admin.ModelAdmin):
+    list_display = (
+        "role",
+    )
+
+@admin.register(WowSecondaryRole)
+class WowSecondaryRoleAdmin(admin.ModelAdmin):
+    list_display = (
+        "secondary_role",
+    )
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "primary_role",
+        "secondary_role",
+        "character_class",
+    )
--- scheduler/models.py
+++ scheduler/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class WowClass(models.Model):
+    character_class = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.character_class
+
+class WowRole(models.Model):
+    role = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.role
+
+class WowSecondaryRole(models.Model):
+    secondary_role = models.CharField(max_length=40, null=True)
+
+    def __str__(self):
+        return self.secondary_role
+
+class Character(models.Model):
+    name = models.CharField(max_length=35)
+    primary_role = models.ForeignKey(
+        WowRole,
+        related_name="character",
+        on_delete=models.CASCADE,
+        )
+    secondary_role = models.ForeignKey(
+        WowSecondaryRole,
+        related_name="character",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    character_class = models.ForeignKey(
+        WowClass,
+        related_name="character",
+        on_delete=models.CASCADE,
+    )
+    def __str__(self):
+        return self.name
